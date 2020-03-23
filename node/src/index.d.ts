1d571d4680c6c7f87095355de417a7ca48369815
selfisekai <today@selfisekai.rocks>
2020-03-24 00:34:13 +0100

    node: decode(), simplified code

--- node/src/index.d.ts
+++ node/src/index.d.ts
@@ -1,4 +1,4 @@
 declare module '@wulkanowy/qr-node' {
   export function encode(password: string, content: string): string;
-  // export function decode(password: string, content: string): string;
+  export function decode(password: string, content: string): string;
 }
--- node/src/index.js
+++ node/src/index.js
@@ -7,27 +7,21 @@ const BASE64 = 'base64';
 const toArrayBuffer = (str) => new TextEncoder().encode(str);
 
 const encode = (password, content) => {
-  const pass = toArrayBuffer(password);
-  const cont = toArrayBuffer(content);
-  const cipher = crypto.createCipheriv(ALGORITHM, pass, Buffer.alloc(0));
-  let encrypted = cipher.update(cont, UTF8, BASE64);
+  const cipher = crypto.createCipheriv(ALGORITHM, toArrayBuffer(password), Buffer.alloc(0));
+  let encrypted = cipher.update(content, UTF8, BASE64);
   encrypted += cipher.final(BASE64);
   return encrypted;
 };
 
-/*
 const decode = (password, content) => {
-  const pass = toArrayBuffer(password);
-  const cont = toArrayBuffer(content);
-  const decipher = crypto.createDecipheriv(ALGORITHM, pass, Buffer.alloc(0));
-  let decrypted = decipher.update(cont, BASE64, UTF8);
+  const decipher = crypto.createDecipheriv(ALGORITHM, toArrayBuffer(password), Buffer.alloc(0));
+  let decrypted = decipher.update(content, BASE64, UTF8);
   decrypted += decipher.final(UTF8);
   return decrypted;
 };
-*/
 
 module.exports = {
   encode,
-  // decode,
+  decode,
   __esModule: true,
 };
