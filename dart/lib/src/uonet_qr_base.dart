a23a644d2ff0774cbc0b60ce69cad19ab863f53f
Zuzanna <unweek@protonmail.com>
2020-08-17 23:57:59 +0200

    Pub-ready version

--- dart/lib/src/uonet_qr_base.dart
+++ dart/lib/src/uonet_qr_base.dart
@@ -2,7 +2,7 @@ import 'package:encrypt/encrypt.dart';
 
 /// Decode a qr code
 String decode(String password, String content) {
-	final key = Key.fromUtf8(password);
+  final key = Key.fromUtf8(password);
   final encrypter = Encrypter(AES(key, mode: AESMode.ecb));
   final enc = Encrypted.fromBase64(content);
   final decoded = encrypter.decrypt(enc);
@@ -10,7 +10,7 @@ String decode(String password, String content) {
 }
 
 String encode(String password, String content) {
-	final key = Key.fromUtf8(password);
+  final key = Key.fromUtf8(password);
   final encrypter = Encrypter(AES(key, mode: AESMode.ecb));
   final encoded = encrypter.encrypt(content);
   return encoded.base64;
